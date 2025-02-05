b00986b0957a324c39a15b6dfceaa55aed88b60b
Wissem Jderi <asqr43641@gmail.com>
2025-02-05 17:08:39 +0100

    implement ingredient addition functionality in Main component

--- my-third-react-tuto-project/src/components/Main.jsx
+++ my-third-react-tuto-project/src/components/Main.jsx
@@ -1,15 +1,29 @@
+import React from "react";
+
 export default function Main() {
+  const [ingredients, setIngredients] = React.useState([]);
+  const sortedIng = ingredients.map((ing) => {
+    return <li key={ing}>{ing}</li>;
+  });
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    setIngredients((prevIngredients) => [...prevIngredients, newIngredient]);
+  }
   return (
     <>
       <main>
-        <form className="add-ingredient-form">
+        <form className="add-ingredient-form" onSubmit={handleSubmit}>
           <input
             type="text"
             aria-label="Add ingredient"
             placeholder="e.g. oregano"
+            name="ingredient"
           />
           <button>Add Ingredient</button>
         </form>
+        <ul>{sortedIng}</ul>
       </main>
     </>
   );
